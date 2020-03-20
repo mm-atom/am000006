bb10bfabb1258b9f45e2d3f1f54af094d6a6dc65
taoqf <tao_qiufeng@126.com>
2020-03-20 09:18:01 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
+import am1 from '@mmstudio/am000001';
+import am3 from '@mmstudio/am000003';
 
-export default function am6(param1: string, param2: string) {
+export default async function nodejs<T>(mm: am1, service: string, msg: unknown) {
+	const body = JSON.stringify(msg);
+	const url = `${am3(mm, 'host')}/sendmessage/${encodeURIComponent(service)}`;
+	const ret = await fetch(url, {
+		body,
+		credentials: 'include',
+		headers: {
+			'Content-Type': 'application/json',
+			'User-Agent': 'feidao-mobile'
+		},
+		method: 'POST'
+	});
+	const status = ret.status;
+	if (status > 0 && status < 300) {
+		return await ret.json() as T;
+	}
+	const err_msg = await ret.text();
+	console.error(`msg status:${ret.statusText}. detail:${err_msg}`);
+	throw new Error(err_msg);
 }
